de7894794b26962b744ab5eafa9047f3e2360c7d
Kkaterynaa <162591694+Kkaterynaa@users.noreply.github.com>
2024-04-10 11:32:22 +0300

    Add files via upload

---
+++ IGadget.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2lab5
+{
+    internal interface IGadget
+    {
+        void Connect();
+        void Disconnect();
+        void Reboot();
+    }
+    class Gadgets : IGadget
+    {
+        public double Price { get; set; }
+
+        public Gadgets(double price)
+        {
+            Price = price;
+        }
+
+        public virtual void Connect()
+        {
+            Console.WriteLine("Connecting components...");
+        }
+
+        public virtual void Disconnect()
+        {
+            Console.WriteLine("Disconnecting components...");
+        }
+
+        public virtual void Reboot()
+        {
+            Console.WriteLine("Rebooting gadget...");
+        }
+    }
+    class Computer : Gadgets
+    {
+        public Computer(double price) : base(price) { }
+
+        public override void Connect()
+        {
+            Console.WriteLine("Connecting computer components...");
+        }
+        public override void Reboot()
+        {
+            Console.WriteLine("Rebooting computer...");
+        }
+    }
+
+    class Laptop : Gadgets
+    {
+        public Laptop(double price) : base(price) { }
+
+        public override void Connect()
+        {
+            Console.WriteLine("Connecting laptop components...");
+        }
+    }
+
+    class DesktopPC : Gadgets
+    {
+        public DesktopPC(double price) : base(price) { }
+        public override void Disconnect()
+        {
+            Console.WriteLine("Disconnecting desktop PC components...");
+        }
+    }
+
+    class NintendoSwitch : Gadgets
+    {
+        public NintendoSwitch(double price) : base(price) { }
+    }
+
+    class Smartphone : Gadgets
+    {
+        public Smartphone(double price) : base(price) { }
+    }
+
+    class IPhone : Smartphone
+    {
+        public IPhone(double price) : base(price) { }
+    }
+
+    class GooglePixel7 : Smartphone
+    {
+        public GooglePixel7(double price) : base(price) { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using _2lab5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Computer computer = new Computer(100000);
+        Console.WriteLine($"Computer price: {computer.Price}");
+        computer.Connect();
+        computer.Reboot();
+        computer.Disconnect();
+
+        Console.WriteLine();
+
+        Laptop laptop = new Laptop(50000);
+        Console.WriteLine($"Laptop price: {laptop.Price}");
+        laptop.Connect();
+        laptop.Reboot();
+        laptop.Disconnect();
+
+        Console.WriteLine();
+
+        DesktopPC desktopPC = new DesktopPC(40000);
+        Console.WriteLine($"Desktop PC price: {desktopPC.Price}");
+        desktopPC.Connect();
+        desktopPC.Reboot();
+        desktopPC.Disconnect();
+
+        Console.WriteLine();
+
+        NintendoSwitch nintendoSwitch = new NintendoSwitch(30000);
+        Console.WriteLine($"Nintendo Switch price: {nintendoSwitch.Price}");
+        nintendoSwitch.Connect();
+        nintendoSwitch.Reboot();
+        nintendoSwitch.Disconnect();
+
+        Console.WriteLine();
+
+        IPhone iPhone = new IPhone(35000);
+        Console.WriteLine($"IPhone price: {iPhone.Price}");
+        iPhone.Connect();
+        iPhone.Reboot(); 
+        iPhone.Disconnect();
+
+        Console.WriteLine();
+
+        GooglePixel7 googlePixel7 = new GooglePixel7(30000);
+        Console.WriteLine($"Google Pixel 7 price: {googlePixel7.Price}");
+        googlePixel7.Connect();
+        googlePixel7.Reboot();
+        googlePixel7.Disconnect();
+    }
+}
